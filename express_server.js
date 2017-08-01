2a0e2f905e268d06e40021615c223e8e38ec5a29
kyle roberts <kyle.w.r@hotmail.com>
2017-08-01 13:58:04 +0000

    added login check for url new page

--- express_server.js
+++ express_server.js
@@ -46,7 +46,8 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
     let templateVars =  {
-      user: users[req.cookies["user_id"]]
+      user: users[req.cookies["user_id"]],
+      res:res
     };
   res.render("urls_new",templateVars);
 });
